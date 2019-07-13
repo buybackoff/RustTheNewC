d3b3f1f2fec5e8507f897696537c10593f1dabfa
buybackoff <vbaybekov@gmail.com>
2019-07-13 15:57:23 +0300

    Add blosc-sys

---
+++ src/rust/blosc-sys/src/lib.rs
@@ -0,0 +1,15 @@
+#![allow(non_upper_case_globals)]
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
+
+// Export bindings
+pub use bindings::*;
+mod bindings;
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
